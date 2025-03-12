a4d36f790aa22a9e7bcf965475fd053c91dd8709
victor <victor.mutuma@student.moringaschool.com>
2025-03-12 13:35:00 +0300

    save completed changes' npm test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "victor is the champion";
+
+document.body.appendChild(newHeader);
